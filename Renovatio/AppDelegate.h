8b85c30d956495264934ef8f3f2d6245eb32bcec
Aly Ibrahim <alyibrahim@Alys-MacBook.local>
2018-07-13 09:11:54 -0400

    Initial Commit

---
+++ Renovatio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Renovatio
+//
+//  Created by Aly Ibrahim on 7/13/18.
+//  Copyright © 2018 Aly Ibrahim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Renovatio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Renovatio
+//
+//  Created by Aly Ibrahim on 7/13/18.
+//  Copyright © 2018 Aly Ibrahim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
